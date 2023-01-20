12f8059d9d4e8c0bd1723d96ab24e4557f1e8706
Itachi746 <122121956+Itachi746@users.noreply.github.com>
2023-01-20 19:45:37 +0530

    Update extra.py

--- Adarsh/bot/plugins/extra.py
+++ Adarsh/bot/plugins/extra.py
@@ -10,7 +10,7 @@ from Adarsh import StartTime
 START_TEXT = """ Your Telegram DC Is : `{}`  """
 
 
-@StreamBot.on_message(filters.regex("🧑‍💻 owner"))
+@StreamBot.on_message(filters.regex("🧑‍💻owner"))
 async def maintainers(b,m):
     try:
        await b.send_message(chat_id=m.chat.id,text="HELLO",quote=True)
@@ -42,7 +42,7 @@ async def follow_user(b,m):
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("Subscribe", url=f"https://telegram.me/Moviezpure")
+                                InlineKeyboardButton("Subscribe❤️", url=f"https://telegram.me/Moviezpure")
                             ]
                         ]
                     ),
